9a2e4c442f006a9419b77e582f03bd9edb312e2f
Mkduan <masonkbduan@gmail.com>
2018-05-28 14:07:27 -0700

    backend push notifications work

--- functions/index.js
+++ functions/index.js
@@ -4,40 +4,38 @@ var fetch = require('node-fetch');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
-//send the push notification 
-exports.sendPushNotification = functions.database.ref('test/').onCreate(event => {
-
-    const root = event.data.ref.root
-    var messages = []
-
-    //return the main promise 
-    return root.child('/users').once('value').then((snapshot) => {
+exports.sendPushNotification = functions.https.onRequest((req, res) => {
+    var messages = [];
+    const latlngCategory = req.query.latlng;
+    console.log("The latlng of the request: " + latlngCategory);
+    return admin.database().ref('/users').once('value').then((snapshot) => {
         snapshot.forEach((childSnapshot) => {
 
             var expoToken = childSnapshot.val().expoToken;
 
-            messages.push({
-                "to": expoToken,
-                "body": "Testing notification"
-            });
-        });
-        //firebase.database then() respved a single promise that resolves
-        //once all the messages have been resolved 
-        return Promise.all(messages);
+            console.log(expoToken);
 
+            if(expoToken) {
+                messages.push({
+                    "to": expoToken,
+                    "body": "Testing notification"
+                });
+            }
+      });
+      console.log("Returning all the messages: " + messages);
+      return Promise.all(messages.toString());
     }).then(messages => {
-            // console.log(messages)
-            return fetch('https://exp.host/--/api/v2/push/send', {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(messages)
-
-            });
-        })
-        .catch(reason => {
-            console.log(reason)
-        })
+        console.log("using expo to send the notifications");
+        return fetch('https://exp.host/--/api/v2/push/send', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(messages)
+        });
+    })
+    .catch(reason => {
+        console.log(reason);
+    });
 });
